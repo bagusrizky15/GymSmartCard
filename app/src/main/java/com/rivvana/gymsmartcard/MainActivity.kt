6467b2e649e4a59b53cd16923e30e08f62e8ccaf
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-11-29 10:31:40 +0700

    add manifest internet permission

--- app/src/main/java/com/rivvana/gymsmartcard/MainActivity.kt
+++ app/src/main/java/com/rivvana/gymsmartcard/MainActivity.kt
@@ -2,10 +2,28 @@ package com.rivvana.gymsmartcard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://mhs.amikom.ac.id")
+        webView.settings.javaScriptEnabled = true
+
+        webView.settings.setSupportZoom(true)
     }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack()
+        }
+        else {
+            super.onBackPressed()
+        }
+    }
+
 }
