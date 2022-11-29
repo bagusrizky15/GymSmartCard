528e31b7ed62c0ba33c89ba70adcc0d4c620dda7
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-11-29 11:53:27 +0700

    add splash screen

--- app/src/main/java/com/rivvana/gymsmartcard/SplashActivity.kt
+++ app/src/main/java/com/rivvana/gymsmartcard/SplashActivity.kt
@@ -1,11 +1,26 @@
 package com.rivvana.gymsmartcard
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        )
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            finish()
+        }, 3000)
     }
 }
